974517b81111f924cb4271ae16757404ab1bd35a
barbafrank <francesco.barbato@studenti.unipd.it>
2020-01-28 16:03:43 +0100

    first try with new pagerank

--- libs/page_rank.py
+++ libs/page_rank.py
@@ -10,8 +10,10 @@ def getDegree(edges):
             degree += e[1]
         return degree
 
+#******************************************************************************
+# other pagerank stuffs
 # the function performs a push operation
-def push(p, r, c, u, neighbours, neighbours_deg, th):
+def push_other(p, r, c, u, neighbours, neighbours_deg, th):
 
     # update the vectors (they are passed as pointers)
     delta = r[u]
@@ -35,7 +37,7 @@ def push(p, r, c, u, neighbours, neighbours_deg, th):
 
     return r_above_th
 
-def approximateSimrank(A, N, D, v, c, epsilon, max_iters=200):
+def approximateSimrank_other(A, N, D, v, c, epsilon, max_iters=200):
 
     # initialize the approximate PageRank vectors
     p = np.zeros(N)
@@ -65,7 +67,7 @@ def approximateSimrank(A, N, D, v, c, epsilon, max_iters=200):
 
         # call the push function to update p and r and
         # to get the list of neighbours to add to the pq
-        r_above_th = push(p, r, c, u, neigh, neigh_deg, th)
+        r_above_th = push_other(p, r, c, u, neigh, neigh_deg, th)
 
         # push the new nodes accordingly to the indicator
         # vector r_above_th returned by push (boolean)
@@ -79,6 +81,83 @@ def approximateSimrank(A, N, D, v, c, epsilon, max_iters=200):
         iters += 1
     # return p
     return p
+
+# define the lazy pagerank
+#*********************************************************+********************
+# the function performs a push operation
+def push(p, r, alpha, u, du, neighbours, neighbours_deg, epsilon):
+
+    # update the vectors (they are passed as pointers)
+    p[u] += alpha*r[u]
+    r[u] *= (1.0 - alpha)/2.
+
+    # initialize the indicator list that tells
+    # approximate pagerank which node needs to be
+    # added into the priority queue for the next
+    # iteration of the algorithm
+    r_above_th = [False]*len(neighbours)
+
+    # update r again and define the values
+    # of the indicator list
+    for i, n in enumerate(neighbours):
+        r[n] += (1.0 - alpha)*r[u]/(2.0*du)
+        # a node v must be added if: r(v)/d(v) >= epsilon
+        if (neighbours_deg[i] == 0 and r[0] != 0) or r[n]/neighbours_deg[i] >= epsilon:
+            r_above_th[i] = True
+
+    return r_above_th
+
+def approximate_page_rank(A, v, alpha, epsilon, max_iters=200, return_residual=False):
+    # compute N
+    N = len(A)
+
+    # initialize the approximate PageRank vectors
+    p = np.zeros(N)
+    r = np.zeros(N)
+    r[v] = 1
+
+    # for some reason pyhton decided to create a min pq
+    # so we push the cost inverted (in this case d/1 = d)
+    pq = []
+    hq.heappush(pq, (getDegree(A[v]), v))
+
+    # precompute the inverse of epsilon for efficiency
+    inv_epsilon = 1/epsilon
+
+    # iterate over the heap as per defined in [1]
+    iters = 0
+    while len(pq)>0 and pq[0][0] <= inv_epsilon and iters<max_iters:
+        u = hq.heappop(pq)[1]
+        neigh = A[u]
+
+        # compute the degrees of the neighbours
+        neigh_deg = [0]*len(neigh)
+        du = 0
+        for i, e in enumerate(neigh):
+            n = e[0]
+            du += e[1]
+            neigh_deg[i] = getDegree(A[n])
+
+        # call the push function to update p and r and
+        # to get the list of neighbours to add to the pq
+        r_above_th = push(p, r, alpha, u, du, neigh, neigh_deg, epsilon)
+
+        # push the new nodes accordingly to the indicator
+        # vector r_above_th returned by push (boolean)
+        for i, flag in enumerate(r_above_th):
+            if flag:
+                n = neigh[i][0]
+                # there can never be a math exception, since if r were
+                # to be 0 then it would have never been inserted in the list
+                hq.heappush(pq, (neigh_deg[i]/r[n]], n))
+
+        iters += 1
+
+    if not return_residual:
+        return p
+    else:
+        return p,r
+
 
 # the function creates the L matrix iterating locally the approximate simrank
 def localPageRank(A, N, D, c, epsilon=1e-5, max_iters=200):
