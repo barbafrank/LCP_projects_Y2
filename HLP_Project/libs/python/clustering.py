e4c1893c6c1a5a65cb41b567134a91239fc40ed5
barbafrank <francesco.barbato@studenti.unipd.it>
2020-02-26 13:28:19 +0100

    added clustering 4

--- HLP_Project/libs/python/clustering.py
+++ HLP_Project/libs/python/clustering.py
@@ -8,7 +8,7 @@ def generate_edges(positions, radius=1, weighted=False):
     N = positions.shape[0]
     idxs = np.arange(N)
     A = [[]]*N
-    
+
     # generate the edgelist
     for i in range(N):
         point = positions[i,:]
@@ -18,7 +18,7 @@ def generate_edges(positions, radius=1, weighted=False):
         for j, neigh in enumerate(neighs):
             if weighted:
                 A[i].append((neigh, 1 + 1./dists[idxs!=i][j]))
-            else:   
+            else:
                 A[i].append(neigh)
 
     # return the generated edgelist
@@ -34,20 +34,20 @@ def build_network(path, sep='\t', radius=1, weighted=False):
 def cluster1(L_norm, centroids, alpha):
     # initialize the output vector with the clusters
     clusters = - np.ones(len(L_norm))
-    
+
     # initialize the heap queues related to the clusters
     queues = []
-    
+
     # insert centroids in the queues, e.g. (expansions, (node, rank))
     for i in range(centroids.size):
         queue = []
         hq.heappush(queue, (0, (centroids[i], 1)))
         queues.append(queue)
-        
+
         # set the centroid cluster
         clusters[centroids[i]] = centroids[i]
-    
-    # iterate until all the nodes are assigned to the clusters   
+
+    # iterate until all the nodes are assigned to the clusters
     exit_ext = False
     while not exit_ext:
         cumulative_length = 0
@@ -56,22 +56,22 @@ def cluster1(L_norm, centroids, alpha):
             for i, node in enumerate(queue):
                 # ensure at least one expansion per node
                 expansions = max(1, np.round(alpha*node[1][1])) # TODO: eventually change the round function
-                
+
                 # modify the queue element with the new expansions
                 queue[i] = (
                     - expansions,
                     (node[1][0], node[1][1])
                 )
-            
+
             # restore the queue
             hq.heapify(queue)
-        
+
             # expand the cluster boundary toward the high ranked nodes
             exit_int = False
             while (not exit_int) and len(queue)>0:
-                # pop a node that should be expanded 
+                # pop a node that should be expanded
                 node = hq.heappop(queue)
-                
+
                 # check the termination condition
                 expansions_opp = node[0]
                 if expansions_opp==0:
@@ -80,7 +80,7 @@ def cluster1(L_norm, centroids, alpha):
                 else:
                     # reduce the remained number of expansions
                     expansions_opp += 1
-                    
+
                     # expand the neighbourhood and assign the new expansions
                     for edge in L_norm[node[1][0]]:
                         #print(neighbour)
@@ -89,7 +89,7 @@ def cluster1(L_norm, centroids, alpha):
                         if clusters[neighbour]<0:
                             #print("assigned")
                             clusters[neighbour] = clusters[node[1][0]]
-                            
+
                             # add the node to the queue
                             rank = edge[1]
                             hq.heappush(
@@ -99,9 +99,9 @@ def cluster1(L_norm, centroids, alpha):
                                     (neighbour, rank)
                                 )
                             )
-            cumulative_length += len(queue) 
+            cumulative_length += len(queue)
         exit_ext = cumulative_length==0
-    
+
     # return the vector with the clusters
     return clusters
 
@@ -109,22 +109,22 @@ def cluster2(L_norm, centroids):
     # initialize queue with the centroids
     # queue element: (weight, (centroid, path_length))
     queue = [(-1., centroid) for centroid in centroids]
-    
+
     # initialize the clusters vector
-    clusters = -np.ones(len(L), dtype=int)
+    clusters = -np.ones(len(L_norm), dtype=int)
     clusters[centroids] = centroids
     iters = 0
-    
+
     # iterate until the queue is not empty
     while len(queue)>0:
-        
+
         # pop the first element
         elem = hq.heappop(queue)
         node = elem[1]
-        
+
         # extract the neighbours
-        neighs = L[node]
-        
+        neighs = L_norm[node]
+
         # for each neighbour
         for neigh in neighs:
             # if it wasn't already assigned
@@ -136,16 +136,16 @@ def cluster2(L_norm, centroids):
                     hq.heappush(queue, ( elem[0]*neigh[1], neigh[0] ))
                 else:
                     hq.heappush(queue, ( elem[0]/2, neigh[0] ))
-                
+
         iters += 1
-    
+
     # return the vector with the clusters
     return clusters
 
 def cluster3(L):
     clusters = -np.ones(len(L), dtype=int)
     parents = np.zeros(len(L), dtype=int)
-    
+
     # for each node and its neighbours
     for idx in range(len(L)):
         # if it was not already assigned to a cluster:
@@ -172,4 +172,77 @@ def cluster3(L):
                     exit_flag = True
 
     # return the clusters
+    return clusters
+
+# A and L are expected in edgelist form, measure is a numpy array
+def cluster4(L_T):
+
+    def getNeighsAvgDegree(node_list, degree):
+        out = 0.
+        for node in node_list:
+            out += degree[node[0]]
+        if len(node_list) > 0:
+            return out/len(node_list)
+        else:
+            return 0
+
+    def cluster_std(nodes, measure):
+        clust_measure = measure[nodes]
+        if clust_measure.shape[0] <= 1:
+            return 0.
+        return 1/(clust_measure.std())
+
+    # compute the average neighbours inDegree
+    # precompute the inDegree of each node
+    inDeg = [0.]*len(L_T)
+    for n, node_list in enumerate(L_T):
+        for _, w in node_list:
+            inDeg[n] += w
+    # compute the actual average, also equal to measure
+    avgNeighsInDeg = [0.]*len(L_T)
+    for i, node_list in enumerate(L_T):
+        avgNeighsInDeg[i] = getNeighsAvgDegree(node_list, inDeg)
+
+    measure = np.array(avgNeighsInDeg)
+    order = np.argsort(measure)
+
+    # node id 0->N-1
+    clusters = np.arange(len(L_T), dtype=int)
+    idxs = np.arange(len(L_T), dtype=int)
+    link_to_follow = [[True for _ in node_list] for node_list in L_T]
+
+    # initialize exit flag
+    flag=True
+    # while the exit flag is not False do
+    while flag:
+        flag = False
+        old_clusts = np.array(clusters)
+        # for each node
+        for n, c in enumerate(clusters[order]):
+            # extract the current cluster
+            n = order[n]
+            curr_cluster = idxs[clusters==c]
+            # follow the link that corresponds to it's maximum relevance
+            weights = np.array([e[1] for e in L_T[n]])[link_to_follow[n]]
+            if weights.shape[0] > 0:
+                max_idx = np.argmax(weights)
+                link_to_follow[n][max_idx] = False
+                neigh = L_T[n][max_idx][0]
+                # make sure that the nodes are in different clusters
+                if c != clusters[neigh]:
+                    neigh_cluster = idxs[clusters==clusters[neigh]]
+                    merge = cluster_std(list(curr_cluster)+list(neigh_cluster), measure)
+                    clust1 = cluster_std(list(curr_cluster), measure)
+                    clust2 = cluster_std(list(neigh_cluster), measure)
+                    if clust1==clust2==0:
+                        split = 0
+                    else:
+                        split = 2*clust1*clust2/(clust1**2+clust2**2)
+                    #print("node:", n, "merge:", merge, "split", split)
+                    if merge > split:
+                        clusters[neigh_cluster] = c
+
+
+        flag = not np.all(clusters==old_clusts)
+
     return clusters
